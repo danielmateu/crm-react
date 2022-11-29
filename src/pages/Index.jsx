2225ed4936c23995c56e551a4e45e35181b11320
Daniel Mateu <danielmateu86@gmail.com>
2022-11-29 12:32:17 +0100

    Mostrando los clientes

--- src/pages/Index.jsx
+++ src/pages/Index.jsx
@@ -1,44 +1,47 @@
 import { useLoaderData } from "react-router-dom";
+import Cliente from "../components/Cliente";
+
+
 
 export const loader = () => {
 
   const clientes = [
     {
-        id: 1,
-        nombre: 'Juan',
-        telefono: 102013313,
-        email: "juan@juan.com",
-        empresa: 'Codigo Con Juan'
+      id: 1,
+      nombre: 'Juan',
+      telefono: 102013313,
+      email: "juan@juan.com",
+      empresa: 'Codigo Con Juan'
     },
     {
-        id: 2,
-        nombre: 'Karen',
-        telefono: 138198313,
-        email: "karen@juan.com",
-        empresa: 'Codigo Con Juan'
+      id: 2,
+      nombre: 'Karen',
+      telefono: 138198313,
+      email: "karen@juan.com",
+      empresa: 'Codigo Con Juan'
     },
     {
-        id: 3,
-        nombre: 'Josue',
-        telefono: 31983913,
-        email: "josue@juan.com",
-        empresa: 'Codigo Con Juan'
+      id: 3,
+      nombre: 'Josue',
+      telefono: 31983913,
+      email: "josue@juan.com",
+      empresa: 'Codigo Con Juan'
     },
     {
-        id: 4,
-        nombre: 'Miguel',
-        telefono: 319381983,
-        email: "miguel@juan.com",
-        empresa: 'Codigo Con Juan'
+      id: 4,
+      nombre: 'Miguel',
+      telefono: 319381983,
+      email: "miguel@juan.com",
+      empresa: 'Codigo Con Juan'
     },
     {
-        id: 5,
-        nombre: 'Pedro',
-        telefono: 1398198938,
-        email: "pedro@juan.com",
-        empresa: 'Codigo Con Juan'
+      id: 5,
+      nombre: 'Pedro',
+      telefono: 1398198938,
+      email: "pedro@juan.com",
+      empresa: 'Codigo Con Juan'
     },
-];
+  ];
 
   return clientes;
 }
@@ -47,13 +50,37 @@ const Index = () => {
 
   const clientes = useLoaderData();
   // console.log(clientes)
-  
+
 
   return (
     <>
       <h1 className="font-black text-4xl text-blue-900">Clientes</h1>
       <p className="mt-3">Administra tu clientes</p>
-    </> 
+
+      {clientes.length ? (
+        <table className="w-full bg-white mt-5 table-auto">
+          <thead className="bg-blue-800 text-white">
+            <tr>
+              <th className="p-2">Cliente</th>
+              <th className="p-2">Contacto</th>
+              <th className="p-2">Acciones</th>
+            </tr>
+          </thead>
+
+          <tbody className="">
+            {clientes.map(cliente => (
+              <Cliente
+                cliente={cliente}
+                key={cliente.id}
+              />
+            ))}
+          </tbody>
+        </table>
+      ) :
+        (
+          <p className="mt-10 text-center">No hay clientes aún</p>
+        )}
+    </>
   )
 }
 
