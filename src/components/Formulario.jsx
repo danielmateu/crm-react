64cb3acd19cb9bd4e7003f73e6f25047dafb1eab
Daniel Mateu <danielmateu86@gmail.com>
2022-12-01 10:57:54 +0100

    autocompletando el formulario para editar

--- src/components/Formulario.jsx
+++ src/components/Formulario.jsx
@@ -14,6 +14,8 @@ const Formulario = ({cliente}) => {
                     className="mt-2 block w-full p-3 bg-gray-50"
                     placeholder="Nombre del Cliente"
                     name="nombre"
+                    defaultValue={cliente?.nombre}
+                
                 />
             </div>
             <div className="mb-4">
@@ -27,6 +29,7 @@ const Formulario = ({cliente}) => {
                     className="mt-2 block w-full p-3 bg-gray-50"
                     placeholder="Empresa del Cliente"
                     name="empresa"
+                    defaultValue={cliente?.empresa}
                 />
             </div>
 
@@ -41,6 +44,7 @@ const Formulario = ({cliente}) => {
                     className="mt-2 block w-full p-3 bg-gray-50"
                     placeholder="Email del Cliente"
                     name="email"
+                    defaultValue={cliente?.email}
                 />
             </div>
 
@@ -55,6 +59,7 @@ const Formulario = ({cliente}) => {
                     className="mt-2 block w-full p-3 bg-gray-50"
                     placeholder="Teléfono del Cliente"
                     name="telefono"
+                    defaultValue={cliente?.telefono}
                 />
             </div>
 
@@ -70,6 +75,7 @@ const Formulario = ({cliente}) => {
                     className="mt-2 block w-full p-3 bg-gray-50 h-40 align-self"
                     placeholder="Notas del Cliente"
                     name="notas"
+                    defaultValue={cliente?.notas}
                 />
             </div>
         </>
--- src/pages/EditarCliente.jsx
+++ src/pages/EditarCliente.jsx
@@ -1,4 +1,8 @@
+import { Form, useLoaderData, useNavigate } from "react-router-dom";
 import { obtenerCliente } from "../../data/clientes"
+import Error from "../components/Error";
+import Formulario from "../components/Formulario";
+
 
 
 
@@ -14,10 +18,48 @@ export async function loader({ params }) {
 
 const EditarCliente = () => {
 
+    const navigate = useNavigate();
+    const cliente = useLoaderData()
+    // console.log(cliente);
 
 
     return (
-        <div>EditarCliente</div>
+
+        <>
+            <h1 className="font-black text-4xl text-blue-900">Editar Cliente</h1>
+            <p className="mt-3">Modifica los datos que necesites del cliente</p>
+
+            <div className="flex justify-end">
+                <button
+                    onClick={() => navigate('/')}
+                    className="bg-blue-600 text-white hover:text-gray-800 px-3 py-1 rounded-xl font-bold uppercase hover:bg-blue-400 ease-in duration-200">
+                    Volver
+                </button>
+            </div>
+
+            <div className="bg-white shadow hover:shadow-none ease-in duration-200 rounded-md md:w-3/4 mx-auto px-5 py-10 my-10">
+                {/* <Formulario/> */}
+
+                {/* {errores?.length && errores.map((error, i) =>(
+                    <Error key={i}>{error}</Error>
+                ))} */}
+
+                <Form 
+                    method="post"
+                    noValidate
+                    >
+
+                    <Formulario
+                        cliente={cliente}/>
+
+                    <input
+                        value='Resitrar cliente'
+                        type="submit"
+                        className="mt-5 w-full bg-blue-600 hover:bg-blue-400 font-bold text-white hover:text-gray-800 ease-in duration-200 rounded-md p-4" />
+
+                </Form>
+            </div>
+        </>
     )
 }
 
